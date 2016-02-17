e5e2008fcd6a381f451832a32d3ccfbb3762f007
Joe Perry <joseph.w.perry@gmail.com>
2016-02-16 22:44:43 -0700

    Basic setup before implementing welcome controller

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,11 @@
 FactoryGirl.define do
+  factory :user do
+    username "MyString"
+    uid "MyString"
+    provider "MyString"
+    email "MyString"
+    image_url "MyString"
+    token "MyString"
+    name "MyString"
+  end
 end
